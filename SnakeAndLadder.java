34641a17627df5a73b98822ba921101319fdfaa2
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-27 17:31:29 +0530

    Added: Repeat till the player reaches the winngin position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,19 +2,22 @@ package Assignment_2;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-		System.out.println("single player at start position is : 0");
-		int dice = (int) ((Math.random() * 6) + 1);
-		System.out.println("to print random number:" + dice);
-		int option = (int) (Math.random() * 3);
+		System.out.println("starting of Snake and Ladder game");
 		int Snake = 1;
 		int Ladder = 2;
-		int presentposition = 5;
-		System.out.println("you are in present position :"+presentposition);
-		System.out.println("to get option is 0 it is no play or if u get 1 it isSnake or to get 2 it is Ladder");
-		System.out.println("if u got option:"+option);
+		int presentposition = 0;
+		System.out.println("starting the game you are in present position :"+ presentposition);
+		while(presentposition < 100) {
+			int dice = (int) ((Math.random() * 6) + 1);
+			System.out.println("to print random number:" + dice);
+			int option = (int) (Math.random() * 3);
+			System.out.println("to get option is 0 it is no play or if u get 1 it isSnake or to get 2 it is Ladder");
+			System.out.println("if u got option:"+option);	
 		if(option == Snake) {
 			presentposition = presentposition - dice;
-			System.out.println("you are on:"+presentposition);
+			if(presentposition < 0)
+				presentposition = 0;
+			System.out.println("you are present position:"+ presentposition);
 		}
 		else if(option == Ladder) {
 			presentposition = presentposition + dice;
@@ -24,4 +27,5 @@ public class SnakeAndLadder {
 			System.out.println("you are present position is:"+ presentposition);
 		}	
 	}
+}
 }
