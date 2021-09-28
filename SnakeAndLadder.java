24a2f672f6b1e6f51c105d71ca137c2a75c48602
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-28 09:42:33 +0530

    Added:Play the game with two players

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,35 +1,55 @@
 package Assignment_2;
 
 public class SnakeAndLadder {
-	public static void main(String[] args) {
-		System.out.println("starting of Snake and Ladder game");
-		int Snake = 1;
-		int Ladder = 2;
-		int presentposition = 0;
-		int DiceRoll = 0;
-		while(presentposition < 100) {
-			DiceRoll ++;
-			int dice = (int) ((Math.random() * 6) + 1);
-			System.out.println("to print random number:" + dice);
-			int option = (int) (Math.random() * 3);
-			System.out.println("to get option is 0 it is no play or if u get 1 it isSnake or to get 2 it is Ladder");
-			System.out.println("if u got option:"+option);	
-		if(option == Snake) {
-			presentposition = presentposition - dice;
-			if(presentposition < 0)
-				presentposition = 0;
-			System.out.println("you are present position:"+ presentposition);
-		}
-		else if(option == Ladder) {
-			presentposition = presentposition + dice;
-			if(presentposition > 100)
-				presentposition = presentposition - dice;
-			System.out.println("you are present position is:"+ presentposition);
-		}
-		else {
-			System.out.println("you are present position is:"+ presentposition);
-		}	
+	int OptionMethod() {
+		int option = (int) (Math.random() * 3);
+		System.out.println("to get option is 0 it is no play or if u get 1 it isSnake or to get 2 it is Ladder");
+		System.out.println("if u got option:"+option);	
+		return option;
 	}
-		System.out.println("number of times die rolled to win the game:"+ DiceRoll);
-}
+		int play() {
+			int presentposition = 0;
+			int NumberOfTimesDiceRolled = 0;
+			while(presentposition < 100) {
+				NumberOfTimesDiceRolled++;
+				SnakeAndLadder Playing = new SnakeAndLadder();
+				int diceRoll = (int)((Math.random() * 6) + 1);
+				System.out.println("to print random number:" + diceRoll);
+				switch(Playing .OptionMethod() ) {
+				case 1:
+					presentposition += diceRoll;
+					if(presentposition > 100)
+						presentposition = presentposition - diceRoll;
+					System.out.println("you are present position:"+ presentposition);
+					int extraRoll = (int)((Math.random() * 6) + 1);
+					System.out.println("The extra roll for getting Ladder and you get number :" + extraRoll);
+					presentposition = presentposition + extraRoll;
+					if(presentposition > 100)
+						presentposition = presentposition - extraRoll;
+					System.out.println("you are present position:"+ presentposition);
+					break;
+				case 2:
+					presentposition = presentposition - diceRoll;
+					if(presentposition < 0)
+						presentposition = 0;
+					System.out.println("you are present position:"+ presentposition);
+					break;
+				default:
+					System.out.println("you are present position:"+ presentposition);
+				}
+			}
+				return NumberOfTimesDiceRolled;
+		}
+		public static void main(String[] args) {
+			SnakeAndLadder game = new SnakeAndLadder();
+			int PlayerOne = game.play();
+			int PlayerTwo = game.play();
+			System.out.println("Number of Two dice rolled by playerone to reach 100:" + PlayerOne);
+			System.out.println("Number of Two dice rolled by playertwo to reach 100:" + PlayerTwo);
+			if(PlayerOne < PlayerTwo) {
+				System.out.println("playerone is winner");
+			}
+			else
+				System.out.println("playerone is winner");
+	}
 }
